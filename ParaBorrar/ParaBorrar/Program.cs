0f7f7cb8ff1066c07e5c758e1d57ca8f9625c50d
Rubén Pérez <ruben.perez@ciberexperis.es>
2019-05-27 12:23:27 +0200

    ParaBorrar - Primera prueba de subida a GitHub

---
+++ ParaBorrar/ParaBorrar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParaBorrar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
