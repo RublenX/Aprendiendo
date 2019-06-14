c34bd271f5eda026f4fbfc063948126600a4fcf5
Rubén Pérez <ruben.perez@ciberexperis.es>
2019-06-14 09:40:24 +0200

    StackoverflowRespuestas - Respuesta a la pregunta 271610 https://es.stackoverflow.com/questions/271610/consumir-servicio-post-c-web-form

---
+++ StackoverflowRespuestas/WinFrmReferenciaExterna/ConsumirPost.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace WinFrmReferenciaExterna
+{
+    /// <summary>
+    /// Respuesta a la pregunta https://es.stackoverflow.com/questions/271610/consumir-servicio-post-c-web-form
+    /// </summary>
+    public class ConsumirPost
+    {
+        public async Task<string> Lanzar(object obj)
+        {
+            try
+            {
+                HttpClient client = new HttpClient()
+                {
+                    BaseAddress = new Uri("http://localhost:62221/"),
+                };
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage respuesta = client.PostAsJsonAsync("/api/Values", obj).Result;
+                if (respuesta.StatusCode == HttpStatusCode.OK)
+                {
+                    // La respuesta es correcta y por ejemplo la retorno como string
+                    return await respuesta.Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR : {ex.Message}");
+            }
+
+            return "KO";
+        }
+    }
+}
