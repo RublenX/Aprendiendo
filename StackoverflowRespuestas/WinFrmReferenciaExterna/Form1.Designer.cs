4173fe887ce7f71364a246773d9c5c97ade3c409
Rubén Pérez <ruben.perez@ciberexperis.es>
2019-06-20 11:23:13 +0200

    StackoverflowRespuestas - Arreglo para que compile bien

--- StackoverflowRespuestas/WinFrmReferenciaExterna/Form1.Designer.cs
+++ StackoverflowRespuestas/WinFrmReferenciaExterna/Form1.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             this.btnLanzar = new System.Windows.Forms.Button();
-            this.dgvTest = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvTest)).BeginInit();
             this.SuspendLayout();
             // 
             // btnLanzar
@@ -45,30 +43,15 @@
             this.btnLanzar.UseVisualStyleBackColor = true;
             this.btnLanzar.Click += new System.EventHandler(this.BtnLanzar_Click);
             // 
-            // dgvTest
-            // 
-            this.dgvTest.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dgvTest.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvTest.Location = new System.Drawing.Point(12, 57);
-            this.dgvTest.Name = "dgvTest";
-            this.dgvTest.RowHeadersWidth = 62;
-            this.dgvTest.RowTemplate.Height = 28;
-            this.dgvTest.Size = new System.Drawing.Size(805, 379);
-            this.dgvTest.TabIndex = 1;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(829, 448);
-            this.Controls.Add(this.dgvTest);
             this.Controls.Add(this.btnLanzar);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvTest)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -76,7 +59,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnLanzar;
-        private System.Windows.Forms.DataGridView dgvTest;
     }
 }
 
--- StackoverflowRespuestas/WinFrmReferenciaExterna/Form1.cs
+++ StackoverflowRespuestas/WinFrmReferenciaExterna/Form1.cs
@@ -8,26 +8,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using WinFrmReferenciaExterna.Entidades;
 
 namespace WinFrmReferenciaExterna
 {
     public partial class Form1 : Form
     {
-        List<UsuarioApp> Usuarios;
 
         public Form1()
         {
             InitializeComponent();
-
-            this.Usuarios = new List<UsuarioApp>
-            {
-                new UsuarioApp { Id = 1, Nombre = "Fulano", Apellido = "De Tal", Alias = "ful", Clave = "0000", Email = "anonimo1@email.es", Tipo = "Acceso A"},
-                new UsuarioApp { Id = 2, Nombre = "Mengano", Apellido = "Pacual", Alias = "men", Clave = "1111", Email = "anonimo2@email.es", Tipo = "Acceso A"},
-                new UsuarioApp { Id = 3, Nombre = "Elena", Apellido = "Moya", Alias = "ele", Clave = "2222", Email = "anonimo3@email.es", Tipo = "Acceso C"}
-            };
-
-            dgvTest.DataSource = this.Usuarios;
         }
 
         private void BtnLanzar_Click(object sender, EventArgs e)
