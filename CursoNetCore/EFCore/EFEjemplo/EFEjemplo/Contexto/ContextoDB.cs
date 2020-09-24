85d7099c9256569f9b755266ab327442cb2502bc
Juan Rubén Pérez Tejada <juanru@outlook.com>
2020-09-24 10:09:36 +0200

    EFEjemplo - Se añade la configuración de conexión con la base de datos creada en local

---
+++ CursoNetCore/EFCore/EFEjemplo/EFEjemplo/Contexto/ContextoDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFEjemplo.Contexto
+{
+    public class ContextoDB : DbContext
+    {
+        public ContextoDB(DbContextOptions<ContextoDB> options) : base(options)
+        {
+            // options se va a pasar por la inyección de dependencias del proyecto
+        }
+
+        // Sobrecarga para el modelo de datos
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
