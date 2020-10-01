6d96e82d807d06173a953f417d4c81626fb212cd
Juan Rubén Pérez Tejada <juanru@outlook.com>
2020-10-01 16:42:12 +0200

    TiendaServicios - Microservicio de CarritoCompras : Se ha añadido el controlador para poder añadir al carrito una nueva compra, además en el Startup se ha añadido MediatR como control para CQRS

--- MicroserviciosAspNetCore/TiendaServicios/TiendaServicios.Api.CarritoCompra/Startup.cs
+++ MicroserviciosAspNetCore/TiendaServicios/TiendaServicios.Api.CarritoCompra/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TiendaServicios.Api.CarritoCompra.Aplicacion;
 using TiendaServicios.Api.CarritoCompra.Persistencia;
 
 namespace TiendaServicios.Api.CarritoCompra
@@ -32,6 +34,8 @@ namespace TiendaServicios.Api.CarritoCompra
             {
                 options.UseMySQL(Configuration.GetConnectionString("ConexionDatabase"));
             });
+
+            services.AddMediatR(typeof(Nuevo.Manejador).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
