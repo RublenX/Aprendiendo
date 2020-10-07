9d6cd19a89c9e19ef1ef6d9195468036751999b0
Juan Rubén Pérez Tejada <juanru@outlook.com>
2020-10-07 13:43:10 +0200

    TiendaServicios - Se introduce la entidad Autor dentro de Libro, se completa la codificación del handler para que devuelva el autor en el libro y se cambia de Staged a Singletor la instancia de la clase en el Startup porque sino no funcionará bien

--- MicroserviciosAspNetCore/TiendaServicios/TiendaServicios.Api.Gateway/MessageHandler/LibroHandler.cs
+++ MicroserviciosAspNetCore/TiendaServicios/TiendaServicios.Api.Gateway/MessageHandler/LibroHandler.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using TiendaServicios.Api.Gateway.InterfaceRemote;
 using TiendaServicios.Api.Gateway.LibroRemote;
 
 namespace TiendaServicios.Api.Gateway.MessageHandler
@@ -15,9 +16,12 @@ namespace TiendaServicios.Api.Gateway.MessageHandler
     {
         private readonly ILogger<LibroHandler> _logger;
 
-        public LibroHandler(ILogger<LibroHandler> logger)
+        private readonly IAutorRemote _autorRemote;
+
+        public LibroHandler(ILogger<LibroHandler> logger, IAutorRemote autorRemote)
         {
             _logger = logger;
+            _autorRemote = autorRemote;
         }
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
@@ -29,6 +33,14 @@ namespace TiendaServicios.Api.Gateway.MessageHandler
                 var contenido = await response.Content.ReadAsStringAsync();
                 var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
                 var resultado = JsonSerializer.Deserialize<LibroModeloRemote>(contenido, options);
+                var responseAutor = await _autorRemote.GetAutor(resultado.AutorLibro ?? Guid.Empty);
+                if (responseAutor.resultado)
+                {
+                    var objetoAutor = responseAutor.autor;
+                    resultado.AutorData = objetoAutor;
+                    var resultadoStr = JsonSerializer.Serialize(resultado);
+                    response.Content = new StringContent(resultadoStr, System.Text.Encoding.UTF8, "application/json");
+                }
             }
 
             _logger.LogInformation($"Este proceso se hizo en {tiempo.ElapsedMilliseconds} ms");
--- MicroserviciosAspNetCore/TiendaServicios/TiendaServicios.Api.Gateway/Startup.cs
+++ MicroserviciosAspNetCore/TiendaServicios/TiendaServicios.Api.Gateway/Startup.cs
@@ -29,12 +29,13 @@ namespace TiendaServicios.Api.Gateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IAutorRemote, AutorRemote>();
+            services.AddSingleton<IAutorRemote, AutorRemote>();
 
             services.AddHttpClient("AutorService", config =>
             {
                 config.BaseAddress = new Uri(Configuration["Services:Autor"]);
             });
+
             // services.AddControllers();
             services.AddOcelot().AddDelegatingHandler<LibroHandler>();
         }
