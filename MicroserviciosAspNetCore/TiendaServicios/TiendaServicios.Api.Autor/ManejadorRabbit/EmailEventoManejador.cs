59ddea3da44035202b4de0fe0265d27e99b5be13
Juan Rubén Pérez Tejada <juanru@outlook.com>
2020-10-08 12:38:49 +0200

    TiendaServicios - Finalización del curso, se añade la información necesaria para enviar al email en el manejador subscrito al evento, se añade la configuración de API Key para SendGrid una vez dado de altra, se añade la referencia al proyectro de Emai desde el Autor y se añade en el Startup la inyección de dependencias

--- MicroserviciosAspNetCore/TiendaServicios/TiendaServicios.Api.Autor/ManejadorRabbit/EmailEventoManejador.cs
+++ MicroserviciosAspNetCore/TiendaServicios/TiendaServicios.Api.Autor/ManejadorRabbit/EmailEventoManejador.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TiendaServicios.Mensajeria.Email.SendGridLibreria.Interface;
+using TiendaServicios.Mensajeria.Email.SendGridLibreria.Modelo;
 using TiendaServicios.RabbitMQ.Bus.BusRabbit;
 using TiendaServicios.RabbitMQ.Bus.EventoQueue;
 
@@ -11,19 +13,37 @@ namespace TiendaServicios.Api.Autor.ManejadorRabbit
     public class EmailEventoManejador : IEventoManejador<EmailEventoQueue>
     {
         private readonly ILogger<EmailEventoManejador> _logger;
+        private readonly ISendGridEnviar _sendGridEnviar;
+        private readonly Microsoft.Extensions.Configuration.IConfiguration _configuration;
 
         public EmailEventoManejador() { }
 
-        public EmailEventoManejador(ILogger<EmailEventoManejador> logger)
+        public EmailEventoManejador(ILogger<EmailEventoManejador> logger, ISendGridEnviar sendGridEnviar, Microsoft.Extensions.Configuration.IConfiguration configuration)
         {
             _logger = logger;
+            _sendGridEnviar = sendGridEnviar;
+            _configuration = configuration;
         }
 
-        public Task Handle(EmailEventoQueue @event)
+        public async Task Handle(EmailEventoQueue @event)
         {
             _logger.LogInformation($"Este es el valor que consumo desde RabbitMQ {@event.Titulo}");
 
-            return Task.CompletedTask;
+            var objData = new SendGridData
+            {
+                Contenido = @event.Contenido,
+                EmailDestinatario = @event.Destinatario,
+                NombreDestinatario = @event.Destinatario,
+                Titulo = @event.Titulo,
+                SendGridAPIKey = _configuration["SendGrid:ApiKey"]
+            };
+            var resultado = await _sendGridEnviar.EnviarEmail(objData);
+
+            if (resultado.resultado)
+            {
+                await Task.CompletedTask;
+                return;
+            }
         }
     }
 }
