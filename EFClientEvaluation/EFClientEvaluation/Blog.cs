5d01c0d1342c2c2ae44fa71f4716bf5f5887ac67
J. Rubén Pérez T <jrperez@ext.mediapro.tv>
2021-12-14 11:27:59 +0100

    Proyecto ejemplo de como las evaluaciones en cliente de Entity Framework pueden influir. Leer el artículo https://docs.microsoft.com/es-es/ef/core/querying/client-eval

---
+++ EFClientEvaluation/EFClientEvaluation/Blog.cs
@@ -0,0 +1,9 @@
+﻿namespace EFClientEvaluation
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public int? Rating { get; set; }
+    }
+}
